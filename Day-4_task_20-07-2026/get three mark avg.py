b9d94a4f636647adc6f640750af83b4a2a40b9f9
VENKATRAMAMOORTHY <86295246+GVRaman17@users.noreply.github.com>
2026-07-20 17:38:11 +0530

    Add files via upload

---
+++ Day-4_task_20-07-2026/get three mark avg.py
@@ -0,0 +1,7 @@
+physic=int(input("Enter a physic mark: "))
+chemistry=int(input("Enter a chemistry mark: "))
+computer=int(input('Enter a computer mark: '))
+total_mark=physic+chemistry+computer
+average=total_mark/3
+print("------------")
+print("Total mark: ",total_mark,"/300","\nAverage mark :",average)
